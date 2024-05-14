45226c3b0581281416442e6516deabd01fdd63c0
V. Raffuzzi(vr339) <vr339@eng.cam.ac.uk>
2024-05-14 16:15:52 +0100

    Added forgotten line

--- CollisionOperator/CollisionProcessors/neutronCEkineticimp_class.f90
+++ CollisionOperator/CollisionProcessors/neutronCEkineticimp_class.f90
@@ -294,6 +294,7 @@ contains
           pTemp % dir = dir
           pTemp % E   = E_out
           pTemp % wgt = wgt
+          pTemp % type      = P_NEUTRON
           pTemp % timeBirth = p % time
 
           call nextCycle % detain(pTemp)
